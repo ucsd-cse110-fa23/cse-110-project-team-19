2407780fb34efe8e7c9567e300dde63d936684d4
BellaReal <101063556+BellaReal@users.noreply.github.com>
2023-11-09 19:30:13 -0800

    moving TranscribeMock to model

--- src/main/java/client/model/TranscribeMock.java
+++ src/main/java/client/model/TranscribeMock.java
@@ -1,10 +1,10 @@
-package client.view.RecordScreen;
+package client.model;
 
 import java.io.IOException;
 import java.net.URISyntaxException;
 
 public class TranscribeMock extends ATranscribe {
-
+    
   public boolean meal;
 
   // should probably be updated to read in audio files this was just a quick last minute solution to test functionality with chatgpt mock
