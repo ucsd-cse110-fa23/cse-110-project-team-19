8a55efeea44ac4a42fab24b2fa4667ff01ea03f0
BellaReal <101063556+BellaReal@users.noreply.github.com>
2023-11-28 20:00:02 -0800

    adding instance of controller to App.java

--- src/main/java/client/App.java
+++ src/main/java/client/App.java
@@ -56,6 +56,14 @@ public class App extends Application {
       view.accountScreen,
       view.mainMenu
     );
+    new AccountScreenController(
+      view,
+      model,
+      createAccountModel,
+      loginModel,
+      view.createAccountError,
+      view.mainMenu
+    );
     //Model model = new Model();
     //Controller controller = new Controller(view, model);
 
