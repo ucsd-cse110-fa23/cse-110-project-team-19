91192f398171330c65b7c5baaf53ad252bd9b4e5
rkwhitlock <reesekwhitlock@gmail.com>
2023-12-05 22:20:31 -0800

    remove duplicate method

--- src/main/java/client/view/RecipeScreen/Footer.java
+++ src/main/java/client/view/RecipeScreen/Footer.java
@@ -52,11 +52,6 @@ public class Footer extends HBox {
   public Button getSaveButton() {
     return saveButton;
   }
-  
-
-  public Button getShareButton() {
-    return shareButton;
-  }
 
   public Button getShareButton() {
     return shareButton;
